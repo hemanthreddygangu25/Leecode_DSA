4d54c34624cdb75b1183bd7ff646b740fcd72cfa
G Hemanth Kumar Reddy <hemanthreddygangu25@gmail.com>
2025-08-03 22:53:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (63.18%) - LeetSync

---
+++ 413-arithmetic-slices/arithmetic-slices.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int ans=0;
+        int n=nums.length;
+        for(int i=0;i<n-1;i++)
+        {
+            int diff=nums[i+1]-nums[i];
+            for(int j=i+2;j<n;j++)
+            {
+                if(nums[j]-nums[j-1]==diff)
+                {
+                    ans++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        return ans;
+        
+    }
+}
