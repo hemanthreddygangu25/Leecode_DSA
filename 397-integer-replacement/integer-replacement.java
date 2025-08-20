2502fc794092e475e9b70a337b33b33bfe0bd35c
G Hemanth Kumar Reddy <hemanthreddygangu25@gmail.com>
2025-08-20 22:10:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (16.16%) - LeetSync

---
+++ 397-integer-replacement/integer-replacement.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int integerReplacement(int n) {
+        long temp = n ;
+        int c = 0;
+        while(temp !=1){
+            if(temp%2==0){
+                temp/=2;
+            }
+            else if(temp==3 || temp%4==1){
+                temp -=1;
+            }
+            else{
+                temp+=1;
+            }
+            c++;
+        }
+        return c;
+        
+    }
+}
