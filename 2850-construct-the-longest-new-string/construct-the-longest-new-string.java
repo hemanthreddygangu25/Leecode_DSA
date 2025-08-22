1ad22ef8fe2bfcd8219ba61c65596f9aaffa7d20
G Hemanth Kumar Reddy <hemanthreddygangu25@gmail.com>
2025-08-22 14:58:36 +0530

    Time: 9 ms (11.03%) | Memory: 44.7 MB (9.65%) - LeetSync

--- 2850-construct-the-longest-new-string/construct-the-longest-new-string.java
+++ 2850-construct-the-longest-new-string/construct-the-longest-new-string.java
@@ -1,6 +1,26 @@
 class Solution {
     public int longestString(int x, int y, int z) {
-        return (Math.min(x, y + 1) + Math.min(x + 1, y) + z) * 2;
-        
+        String s="";
+        for(int i=0;i<z;i++)
+        {
+            s+="AB";
+        }
+        int mini=Math.min(x,y);
+        for(int j=0;j<mini;j++)
+        {
+            s+="AA";
+            s+="BB";
+            x--;
+            y--;
+        }
+        if(x!=0)
+        {
+            s+="AA";
+        }
+        if(y!=0)
+        {
+            s+="BB";
+        }
+        return s.length();
     }
 }
