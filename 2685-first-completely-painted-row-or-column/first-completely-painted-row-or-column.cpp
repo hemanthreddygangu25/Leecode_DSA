f6519594c85469fb5701b8857f2f03e5aba02ca6
G Hemanth Kumar Reddy <hemanthreddygangu25@gmail.com>
2025-08-30 16:53:28 +0530

    Time: 261 ms (15.18%) | Memory: 165.6 MB (70.68%) - LeetSync

---
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        map<int,int>mpp;
+        for(int i=0;i<arr.size();i++){
+            mpp[arr[i]]=i;
+        }
+        int m=mat.size();
+        int n=mat[0].size();
+        int maxi=0;
+        vector<int>ans;
+        for(int i=0;i<m;i++){
+            int sum=0;
+            for(int j=0;j<n;j++){
+              sum=max(sum,mpp[mat[i][j]]);
+            }
+            ans.push_back(sum);
+        }
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=0;j<m;j++){
+                sum=max(sum,mpp[mat[j][i]]);
+            }
+            ans.push_back(sum);
+        }
+        sort(ans.begin(),ans.end());
+return ans[0];
+    }
+};
