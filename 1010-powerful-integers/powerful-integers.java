90a1b0c4d3acf8e6e7cf1e5e9068199037f57b61
G Hemanth Kumar Reddy <hemanthreddygangu25@gmail.com>
2025-09-05 00:12:01 +0530

    Time: 1 ms (100.00%) | Memory: 41.5 MB (40.00%) - LeetSync

---
+++ 1010-powerful-integers/powerful-integers.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        Set<Integer> ans = new HashSet<>();
+        for (int xi = 1; xi < bound; xi *= x) {
+            for (int yj = 1; xi + yj <= bound; yj *= y) {
+                ans.add(xi + yj);
+                if (y == 1) break;
+            }
+            if (x == 1) break;
+        }
+        return new ArrayList<Integer>(ans);
+    }
+}
