744209b7775989948bc4e54ec7b19e068c922b35
G Hemanth Kumar Reddy <hemanthreddygangu25@gmail.com>
2025-09-11 15:38:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (94.82%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
@@ -1,23 +1,18 @@
 class Solution {
-    public int balancedStringSplit(String s) {
-      int count=0,ans=0;
-      for(int i:s.toCharArray())
-      {
-        if(i=='R')
-        {
-          count++;
-        }
-        else if(i=='L')
-        {
-          count--;
-        }
-        if(count==0)
-        {
-          ans++;
-
-        }
+  public int balancedStringSplit(String s) {
+    int count = 0, ans = 0;
+    for (int i : s.toCharArray()) {
+      if (i == 'R') {
+        count++;
+      } else if (i == 'L') {
+        count--;
       }
-      return ans;
-        
+      if (count == 0) {
+        ans++;
+
+      }
     }
+    return ans;
+
+  }
 }
