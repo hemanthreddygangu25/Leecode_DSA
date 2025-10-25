b5acf97e4af53229821b998c184a1123f49e0194
G Hemanth Kumar Reddy <hemanthreddygangu25@gmail.com>
2025-10-25 14:09:23 +0530

    Time: 1 ms (56.85%) | Memory: 40.6 MB (32.47%) - LeetSync

---
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int totalMoney(int n) {
+     int sum=0,i=1,icount=0;
+     int w=1,wcount=1;
+     while(i<=n)
+     {
+      if(icount==7)
+      {
+        wcount++;
+        w=wcount;
+        icount=0;
+      }
+      icount++;
+      sum+=w;
+      w++;
+      i++;
+
+     }
+     return sum;
+        
+    }
+}
